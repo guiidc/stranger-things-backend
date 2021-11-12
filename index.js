0fb21462facb74e6bef169d6887078fd93ac1a4b
Guilherme da Costa <guilhermedc93@gmail.com>
2021-11-12 20:16:23 -0300

    requisito 1 ao 6 concluido

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+let { UPSIDEDOWN_MODE } = process.env;
+UPSIDEDOWN_MODE = UPSIDEDOWN_MODE === true;
+
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
